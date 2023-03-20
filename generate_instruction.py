a3615a899d52d394317f29e09445d208025fad5f
davidliaos <122007821+davidliaos@users.noreply.github.com>
2023-03-20 15:48:42 -0400

    Update model to gpt-4
    Changed cl to gpt-4 since we are trying to retrain for gpt-4.

--- generate_instruction.py
+++ generate_instruction.py
@@ -5,7 +5,7 @@ run:
 python -m generate_instruction generate_instruction_following_data \
   --output_dir ./ \
   --num_instructions_to_generate 10 \
-  --model_name="text-davinci-003" \
+  --model_name="gpt-4" \
 """
 import time
 import json
